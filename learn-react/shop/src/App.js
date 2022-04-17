eb34d32f282e0fb60721870762556df2d0f507a9
ymiru0324 <ymiru0324@naver.com>
2022-04-17 21:54:39 +0900

    project set

---
+++ learn-react/shop/src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable */ // 터미널에 뜨는 warning eslint 제거
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
